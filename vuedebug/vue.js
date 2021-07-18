41155bde169da6d72f2b00455a1c64b9ae9a7412
weidehai <243395655@qq.com>
2021-07-19 01:31:23 +0800

    auto push 20210719 13118

--- vuedebug/vue.js
+++ vuedebug/vue.js
@@ -3438,7 +3438,7 @@ console.log("Vue execute");
     var parentVnode = (vm.$vnode = options._parentVnode); // the placeholder node in parent tree
     var renderContext = parentVnode && parentVnode.context;
     vm.$slots = resolveSlots(options._renderChildren, renderContext);
-    vm.$scopedSlots = emptyObject;
+    vm.$scopedSlots = emptyObject;	
     // bind the createElement fn to this instance
     // so that we get proper render context inside it.
     // args order: tag, data, children, normalizationType, alwaysNormalize
